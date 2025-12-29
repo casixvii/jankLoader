04a5c989e332bc96b7fe347fdada57bb05c23615
Casix <mvpcasix@gmail.com>
2025-12-29 20:18:09 +0500

    Update jankloader.lua

--- jankloader.lua
+++ jankloader.lua
@@ -21,7 +21,7 @@ function jankloader.loadMap()
 	local file2 = file:read()
 	-- print(file2)
 	jankloader.mapInfo.et.x = file2:match("b[^%[]*%[([^,]+)")
-	jankloader.mapInfo.et.y = file2:match("b[^%[]*%[[^,]+,([^%]]+)%]%$")
+	jankloader.mapInfo.et.y = file2:match("b[^%[]*%[[^,]+,([^%]]+)%]")
 	for wc = 1, 20 do
 		local val = string.char(wc+33)
 		-- this is less jank:tm:y and will break any map files made before this
@@ -31,7 +31,7 @@ function jankloader.loadMap()
 			jankloader.mapInfo.walls[wc].b = file2:match("%".. val ..".- (.-) .- .- .-")
 			jankloader.mapInfo.walls[wc].l = file2:match("%".. val ..".- .- (.-) .- .-")
 			jankloader.mapInfo.walls[wc].r = file2:match("%".. val ..".- .- .- (.-) .-")
-			jankloader.mapInfo.walls[wc].r = file2:match("%".. val ..".- .- .- .- (.-)")
+			jankloader.mapInfo.walls[wc].s = file2:match("%".. val ..".- .- .- .- (.-)")
 		end
 	end
 	file:close()
