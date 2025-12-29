cdb54485484f7d698eafc6d47bf8e7fbf6302801
Casix <mvpcasix@gmail.com>
2025-12-29 20:08:33 +0500

    Update jankloader.lua

--- jankloader.lua
+++ jankloader.lua
@@ -25,11 +25,14 @@ function jankloader.loadMap()
 	for wc = 1, 20 do
 		local val = string.char(wc+33)
 		-- this is less jank:tm:y and will break any map files made before this
-		jankloader.mapInfo.walls[wc].t = file2:match("%".. val .."(.-) .- .- .- .-")
-		jankloader.mapInfo.walls[wc].b = file2:match("%".. val ..".- (.-) .- .- .-")
-		jankloader.mapInfo.walls[wc].l = file2:match("%".. val ..".- .- (.-) .- .-")
-		jankloader.mapInfo.walls[wc].r = file2:match("%".. val ..".- .- .- (.-) .-")
-		jankloader.mapInfo.walls[wc].r = file2:match("%".. val ..".- .- .- .- (.-)")
+		if file2:match("%".. val .."(.-) .- .- .- .-") == nil then
+		else
+			jankloader.mapInfo.walls[wc].t = file2:match("%".. val .."(.-) .- .- .- .-")
+			jankloader.mapInfo.walls[wc].b = file2:match("%".. val ..".- (.-) .- .- .-")
+			jankloader.mapInfo.walls[wc].l = file2:match("%".. val ..".- .- (.-) .- .-")
+			jankloader.mapInfo.walls[wc].r = file2:match("%".. val ..".- .- .- (.-) .-")
+			jankloader.mapInfo.walls[wc].r = file2:match("%".. val ..".- .- .- .- (.-)")
+		end
 	end
 	file:close()
 end
