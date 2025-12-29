b3f6781e9e74bcee6ca6d90ae3cd1c48b65e6150
Casix <mvpcasix@gmail.com>
2025-12-29 23:17:35 +0500

    Uses unicode instead of ASCII to load maps

--- jankloader.lua
+++ jankloader.lua
@@ -1,4 +1,4 @@
-_G.jankloader = {
+local jankloader = {
 	currentMap = ".\\maps\\00.jma",
 	mapInfo = {
 		walls = {
@@ -22,8 +22,8 @@ function jankloader.loadMap()
 	-- print(file2)
 	jankloader.mapInfo.et.x = file2:match("b[^%[]*%[([^,]+)")
 	jankloader.mapInfo.et.y = file2:match("b[^%[]*%[[^,]+,([^%]]+)%]")
-	for wc = 1, 20 do
-		local val = string.char(wc+33)
+	for wc = 0, 96-1 do
+		local val = utf8.char(wc+33)
 		-- this is less jank:tm:y and will break any map files made before this
 		if file2:match("%".. val .."(.-) .- .- .- .-") == nil then
 		else
